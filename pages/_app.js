bdf89665746e9551967009b38ac5ac984b96b5ca
Ansh Kumar <anshkumar1399@gmail.com>
2025-03-19 22:48:53 +0530

    first

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+
+import { NavBar } from '../components/componentsindex';
+
+const MyApp = ({ Component, pageProps }) =>
+<div>
+    <NavBar />
+    <Component {...pageProps} />
+</div>
+
+export default MyApp
